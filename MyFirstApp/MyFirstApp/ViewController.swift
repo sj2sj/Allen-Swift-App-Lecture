5cd550559e4e215bb7c1657184e85a3520965962
sj2sj <waterttug@naver.com>
2023-03-29 16:26:16 +0900

    add myFirstApp

---
+++ MyFirstApp/MyFirstApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by 뜌딩 on 2023/03/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
